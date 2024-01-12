70851f5c41f3377acf4af60d6c62056a8b130834
liuyao <liuyao@zhito.com>
2024-01-12 13:52:24 +0800

    add code
    Change-Id: I5081b4491b8c4ce708f5a150146e66c8045aaf2c

--- main.cc
+++
@@ -1,9 +0,0 @@
-#include "iostream"
-
-#include <opencv2/opencv.hpp>
-
-int main() {
-  cv::Mat img = cv::imread("./data/image.png");
-  std::cout << "img size " << img.size() << std::endl;
-  return 0;
-}
